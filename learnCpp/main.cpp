90aebaf743180a1b865cb03847b2b5ef45768294
Redouane Elkaboussi <elkaboussi@pm.me>
2023-05-25 16:11:01 +0100

    add cmake ignore

--- learnCpp/main.cpp
+++ learnCpp/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
-std::string upperCase(char *str) {
+std::string upperCase(std::string str) {
     std::string buffer;
 
-    for (int i = 0; str[i]; i++) {
+    for (int i = {0}; str[i]; i++) {
         buffer += char(toupper(str[i]));
     }
     return buffer;
@@ -15,7 +15,14 @@ int main(int argc, char *argv[]) {
         std::cout << "Usage: " << argv[0] << " <param>" << std::endl;
         return 1;
     }
-    std::cout << upperCase(argv[1]) << std::endl;
-
+    std::string buffer;
+    while (true) {
+        std::getline(std::cin, buffer);
+        if (std::cin.eof())
+            break;
+        if (buffer == "EXIT")
+            break;
+        std::cout << upperCase(buffer) << std::endl;
+    }
     return 0;
 }
--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <cctype>
 
-using namespace std;
-
 int main(int argc, char **argv) {
-    if (argc < 2)
-    {
+    if (argc < 2) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return 0;
     }
     for (int i = 1; i < argc; ++i) {
         for (int j = 0; argv[i][j]; j++) {
-            cout << static_cast<char>(toupper(argv[i][j]));
+            std::cout << char(toupper(argv[i][j]));
         }
-        cout << endl;
+        std::cout << std::endl;
     }
     return 0;
 }
