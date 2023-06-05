71348645467c217453614b8b48f42cef221a6d10
relkabou <elkaboussi@pm.me>
2023-06-05 18:39:12 +0100

    setup workspace

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Fixed.hpp"
+
+
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Deconstruct called" << std::endl;
+}
+
+// static members are shared by all instances
+// they must be initialized outside any constructor
+Fixed::Fixed(Fixed &obj) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	fixedPointNumberValue = obj.fixedPointNumberValue;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	// TODO: body
+	return 0;
+}
+
+void Fixed::setRawBits(const int raw) {
+	(void)raw;
+	std::cout << "in setRaws" << std::endl;
+	// TODO: body
+}
