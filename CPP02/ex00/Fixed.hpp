813efe60a7306762e6f54d7aa3112b5ad3e4e262
relkabou <elkaboussi@pm.me>
2023-06-05 18:39:12 +0100

    setup workspace

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -5,8 +5,23 @@
 #ifndef POOL_FIXED_HPP
 #define POOL_FIXED_HPP
 
+#include <string>
+#include <iostream>
 
 class Fixed {
+public:
+	Fixed();
+	Fixed (Fixed &obj);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+private:
+	int					fixedPointNumberValue;
+	static const int	fractionalBits;
+
+public:
+
 
 };
 
