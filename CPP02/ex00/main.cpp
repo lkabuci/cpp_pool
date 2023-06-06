bd2624edc15c9641529456b57cc11ce3d11f5a0d
relkabou <elkaboussi@pm.me>
2023-06-06 15:02:30 +0100

    done: cpp02/ex02

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -9,6 +9,7 @@ int main( void ) {
 	Fixed b( a );
 	Fixed c;
 	c = b;
+
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
