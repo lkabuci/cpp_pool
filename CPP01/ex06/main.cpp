c48e70cae7dc3e50f7ab3e2a52978501b31fb0b1
Redouane Elkaboussi <elkaboussi@pm.me>
2023-06-18 12:34:01 +0100

    bugfix in CPP01/ex04 and CPP01/ex06

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -3,14 +3,15 @@
 //
 
 #include "Harl.hpp"
+#include <cstdlib>
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		std::cerr << "Error: invalid args" << std::endl;
-		return EXIT_FAILURE;
-	}
+    if (argc != 2) {
+        std::cerr << "Error: invalid args" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	Harl harl;
-	harl.complain(std::string(argv[1]));
-	return 0;
+    Harl harl;
+    harl.complain(std::string(argv[1]));
+    return 0;
 }
